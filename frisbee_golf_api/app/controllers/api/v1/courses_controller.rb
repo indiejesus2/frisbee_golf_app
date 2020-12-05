78fe57fa083e77676451c020dd305c4740d97c88
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-12-05 13:41:00 -0500

    WIP: Added update controller, updateRating in ApiService.

--- frisbee_golf_api/app/controllers/api/v1/courses_controller.rb
+++ frisbee_golf_api/app/controllers/api/v1/courses_controller.rb
@@ -2,11 +2,27 @@ class Api::V1::CoursesController < ApplicationController
 
     def index
         @courses = Api::V1::Course.all
-        @courses.each do |course|
+        @courses.map do |course|
             course.zero
-
         end
         render json: @courses, except: [:created_at, :updated_at]
     end
+
+    def show
+    end
+
+    def update
+        @course = Api::V1::Course.find_by_id(params[:id])
+        debugger
+        @course.update(tally: @course.tally + 1, votes: @course.votes += course_params)
+        debugger
+    end
+
+    private
+
+    def course_params
+        params.require(:course).permit(:vote)
+    end
+
 
 end
