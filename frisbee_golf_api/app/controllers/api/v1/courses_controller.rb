e84f85d953f53880194c90add2d5d975976c3aa9
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-12-05 12:48:39 -0500

    WIP: Add calculateRating function.

--- frisbee_golf_api/app/controllers/api/v1/courses_controller.rb
+++ frisbee_golf_api/app/controllers/api/v1/courses_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::CoursesController < ApplicationController
     def index
         @courses = Api::V1::Course.all
         @courses.each do |course|
-            course.rating
+            course.zero
+
         end
         render json: @courses, except: [:created_at, :updated_at]
     end
--- frisbee_golf_api/app/models/api/v1/course.rb
+++ frisbee_golf_api/app/models/api/v1/course.rb
@@ -1,11 +1,20 @@
 class Api::V1::Course < ApplicationRecord
     has_many :api_v1_comments
 
-    def rating
+    def zero
         if self.votes == nil
             self.votes = 0
             self.tally = 0
         end
     end
+
+    # def rating
+    #     debugger
+    #     if self.votes != 0 && self.tally != 0
+    #         self.votes % self.tally
+    #     else
+    #         0
+    #     end
+    # end
 
 end
