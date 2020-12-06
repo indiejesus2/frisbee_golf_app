1cc25c3e93503269b1af06bac1454bbd34567c93
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-12-06 17:13:58 -0500

    WIP: Created a nested resource between Courses and Comments. Added Update to the Comments Controller. Next to do, add comment to page.

--- frisbee_golf_api/app/controllers/api/v1/comments_controller.rb
+++ frisbee_golf_api/app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::CommentsController < ApplicationController
+
+    def index
+    end
+
+    def create
+        @course = Api::V1::Course.find_by_id(params[:course_id])
+        @course.comments.create(comment_params)
+        render json: @course, except: [:created_at, :updated_at]
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:review)
+    end
 end
--- frisbee_golf_api/config/routes.rb
+++ frisbee_golf_api/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   end
   namespace :api do
     namespace :v1 do
-      resources :courses
+      resources :courses do
+        resources :comments
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
